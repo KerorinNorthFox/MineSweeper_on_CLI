49faad723531043636a27581274e24f9b29697f5
kerorinnf <kitakitsune.gingitsune.8220A@gmail.com>
2023-03-09 12:47:32 +0900

    start画面作成

--- src/minesweeper.nim
+++ src/minesweeper.nim
@@ -4,10 +4,18 @@ import
   std/strutils,
   ./blocks
 
+var tb: TerminalBuffer
+
 type MineSweeper* = ref object
+  blocksSeq: seq[Blocks]
   blcNum*: int
 
-proc inputSetting(self:MineSweeper, tb:var TerminalBuffer): void =
+proc clearTerminal(): void =
+  tb.clear()
+  tb.display()
+
+proc inputSetting(self:MineSweeper): void =
+  clearTerminal()
   tb.setForegroundColor(fgWhite, true)
   tb.drawRect(0, 0, 50, 5)
   tb.write(2, 1, "<-Initial Setting->")
@@ -38,8 +46,6 @@ proc inputSetting(self:MineSweeper, tb:var TerminalBuffer): void =
         number = num.parseInt
         if number >= 5 and number <= 20:
           self.blcNum = number
-          tb.clear()
-          tb.display()
           return
       except ValueError: continue
 
@@ -53,8 +59,24 @@ proc inputSetting(self:MineSweeper, tb:var TerminalBuffer): void =
     tb.write(3, 4, " ".repeat(45))
     tb.write(3, 4, num)
 
-proc init*(_:type MineSweeper, tb:var TerminalBuffer): MineSweeper =
-  var game = MineSweeper(blcNum:0)
-  game.inputSetting(tb)
+proc setting(self:MineSweeper): void =
+  discard
 
-  
+proc start(self:MineSweeper): void =
+  clearTerminal()
+  var
+    halfWidth: int = tb.width div 2
+    halfHeight: int = tb.height div 2
+    text: string = "To the start of the game..."
+  tb.drawRect(halfWidth-20, halfHeight-3, halfWidth+20, halfHeight+1)
+  for count in countdown(3, 1):
+    tb.write(halfWidth-13, halfHeight-1, text, $count)
+    tb.display()
+    sleep(1000)
+
+proc init*(_:type MineSweeper, terminalbuffer:var TerminalBuffer): MineSweeper =
+  tb = terminalbuffer
+  var game = MineSweeper(blcNum:0)
+  game.inputSetting()
+  game.setting()
+  game.start()
